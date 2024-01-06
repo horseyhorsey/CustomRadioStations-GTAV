af5af1f400a260db91db77639c1b257cc5794694
dave <horsepin@protonmail.com>
2024-01-06 14:40:21 +0000

    constants

---
+++ CustomRadioStations/Common/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomRadioStations
+{
+    public class Constants
+    {
+        /// <summary>constants for paths for config, logging</summary>
+        public const string
+            MAIN_PATH = @"scripts\Custom Radio Stations\",
+            ICON_BG_PATH = @"scripts\Custom Radio Stations\iconbg.png",
+            ICON_HL_PATH = @"scripts\Custom Radio Stations\iconhl.png",
+            NLOG_PATH = @"scripts\Custom Radio Stations\NativeStations.log",
+            NLOG_CFG_PATH = @"scripts\Custom Radio Stations\NativeWheels.cfg",
+            INI_PATH = @"scripts\Custom Radio Stations\settings.ini";
+    }
+}
